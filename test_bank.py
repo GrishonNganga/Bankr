2f8f90ba1f1080eab581e86f21dfa5098ec60849
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-07-16 10:41:14 +0300

    Add Message to guide.

--- test_bank.py
+++ test_bank.py
@@ -19,46 +19,62 @@ class TestUser(unittest.TestCase):
         self.assertEqual(a_new_user.id, 31390865)
 
     def test_account(self):
+        #Setup & Perform
         self.user_account = Account(self.a_new_user.id, 2000)
 
+        #Test
         self.assertEqual(self.user_account.user_id, self.a_new_user.id)
         self.assertEqual(self.user_account.amount, 2000)
         
 
     def test_user_create_account(self):
+        #Setup
         self.a_new_user.create_account(2000)
 
+        #Perform
         self.a_new_user.accounts
+
+        #Test
         self.assertEqual(len(self.a_new_user.accounts), 1)
 
     def test_user_debit_account(self):
+        #Setup
         self.a_new_user.create_account(2000)
         account = self.a_new_user.accounts[0]    
 
+        #Perform
         account.debit(200)        
         
+        #Test
         self.assertEqual(self.a_new_user.accounts[0].amount, 2200)
 
-    def test_user_credit_account(self):
+    def test_user_create_account(self):
+        #Setup & Perform
         self.a_new_user.create_account(2000)
 
-        self.a_new_user.accounts
+        #Test
         self.assertEqual(len(self.a_new_user.accounts), 1)
 
-    def test_user_debit_account(self):
+    def test_user_credit_account(self):
+        #Setup
         self.a_new_user.create_account(2000)
         account = self.a_new_user.accounts[0]    
 
+        #Perform
         account.credit(200)        
         
+        #Test
         self.assertEqual(self.a_new_user.accounts[0].amount, 1800)
 
 
     def test_delete_account(self):
+        #Setup
         self.a_new_user.create_account(100)
         
+        #Perform
         self.a_new_user.delete_account(self.a_new_user.accounts[0])
 
+        #Test
         self.assertEqual(len(self.a_new_user.accounts), 0)
 
 if __name__ == '__main__':
